2b1c8d6f514c7c7f067c30fb8026d7305cbd5f8e
Augustine2630 <maksim2630@gmail.com>
2023-06-12 18:56:02 +0300

    new back

--- app/src/main/java/com/aug/labas/MenuActivity.java
+++ app/src/main/java/com/aug/labas/MenuActivity.java
@@ -1,13 +1,20 @@
 package com.aug.labas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -16,7 +23,14 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         final TextView my_text = (TextView) findViewById(R.id.my_text);
+        ConstraintLayout layout = findViewById(R.id.idRLContainer);
         my_text.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/PenguinAttackCyrillic.otf"));
+        int orientation = this.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            layout.setBackground(getResources().getDrawable(R.drawable.amogus_land));
+        } else {
+            layout.setBackground(getResources().getDrawable(R.drawable.background));
+        }
     }
 
     @Override
@@ -24,6 +38,10 @@ public class MenuActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -39,6 +57,10 @@ public class MenuActivity extends AppCompatActivity {
             return true;
         } else if (item.getItemId() == R.id.exit) {
             finish();
+            return true;
+        } else if (item.getItemId() == R.id.catToast) {
+            Toast.makeText(getApplicationContext(),
+                    "Это крутой функционал!", Toast.LENGTH_LONG).show();
             return true;
         } else {
             return super.onOptionsItemSelected(item);
